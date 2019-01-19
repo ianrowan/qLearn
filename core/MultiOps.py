6871d5fa162e28cf897abd68c94ed0abb546b033
ian <i.rowan299@gmail.com>
2019-01-18 21:16:25 -0600

    Added Multi entangled tensor product

--- core/MultiOps.py
+++ core/MultiOps.py
@@ -12,14 +12,27 @@ def tensor_product(*args):
 
     if type(args[0]) == list:
         args = args[0]
+    args = list(args)
 
     def __product(x1, x2):
         return np.asarray([i*j for i in x1 for j in x2])
 
-    t_product = np.zeros(shape=[int(math.pow(2,len(args))), 1])
-    t_product[:4] = __product(args[0].superposition(), args[1].superposition())
+    t_product = np.zeros(shape=[int(math.pow(2, len(args))), 1])
+
+    entangled = system_elements_entangled(args)
+    if entangled[1] > 0:
+        systems = [CNOT(entangled[0][n*2], entangled[0][n*2+1]) for n in range(int(entangled[1]))]
+        for p in range(len(systems)):
+            t_product[:int(math.pow(4, p+1))] = __product(t_product[:int(math.pow(4, p))], systems[p]) \
+                if p > 0 else systems[0]
+        args = entangled[0]
+        num = int(entangled[1] * 2)
+    else:
+        t_product[:4] = __product(args[0].superposition(), args[1].superposition())
+        num = 2
+
     if len(args) > 2:
-        for k in range(2, len(args)):
+        for k in range(num, len(args)):
             t_product[:int(math.pow(2, k+1))] = __product(t_product[:int(math.pow(2, k))], args[k].superposition())
     return t_product
 
@@ -29,9 +42,9 @@ def CNOT(q1, q2):
                        [0, 0, 0, 1],
                        [0, 0, 1, 0]])
     q_system = tensor_product_simple(q1, q2)
-    #Todo: log/track entangelment
+
     q1.entangled_with = id(q2)
-    q2.entangled_With = id(q1)
+    q2.entangled_with = id(q1)
 
     return np.matmul(gate, q_system)
 
@@ -41,30 +54,35 @@ def are_entangled(q1, q2):
 def system_elements_entangled(*args):
     if type(args[0]) == list:
         args = args[0]
+    args = list(args)
     e = []
     for i in args:
         for j in args:
             if are_entangled(i, j):
                 e.append(i)
                 e.append(j)
-                del i, j
+                args.pop(args.index(i))
+                args.pop(args.index(j))
     num_entangled = len(e)/2
-    e.append(args)
+    e.extend(args)
     return e, num_entangled
 
 
-q1 = Qubit()
-q1.H()
-print(q1.superposition())
-q2 = Qubit()
-q2.H()
-q3 = Qubit()
-print(q3.is_entangled())
-print(CNOT(q2, q3))
-print(are_entangled(q1, q3))
+Q1 = Qubit()
+Q1.H()
+#print(q1.superposition())
+Q2 = Qubit()
+Q2.H()
+Q3 = Qubit()
+Q4 = Qubit()
+Q4.H()
+Q5 = Qubit()
+Q4.X()
+#print(tensor_product_simple(q2, q3))
+CNOT(Q2, Q3)
+CNOT(Q1, Q4)
+print(tensor_product(Q1, Q2, Q3, Q4, Q5))
 
-print(system_elements_entangled(q1, q2, q3))
-#print(tensor_product(q1, q2, q3))
 x = []
 for i in range(25):
     q = Qubit()
@@ -72,7 +90,7 @@ for i in range(25):
         q.X()
     q.H()
     x.append(q)
-#print(tensor_product(x))
+print(tensor_product(x))
 
 
 
